3f35e8a128e960b84a7152b1b9da010ea0bb6d96
Sergio Nicolas Huaman Cheng <sergiohuamanch15@gmail.com>
2024-03-11 21:05:06 -0500

    changes in vercel.json and index port configuration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import app from './app.js'
-import { sequelize } from './database/db.js'
+const express = require("express");
+const app = express();
 
-async function main() {
-    try {
-        await sequelize.sync({force:false})
-        app.listen(4000);
-        console.log('Server RUN!');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-main();
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
+
+// import app from './app.js'
+// import { sequelize } from './database/db.js'
+
+// async function main() {
+//     try {
+//         await sequelize.sync({force:false})
+//         app.listen(4000);
+//         console.log('Server RUN!');
+//     } catch (error) {
+//         console.error('Unable to connect to the database:', error);
+//     }
+// }
+
+// main();
