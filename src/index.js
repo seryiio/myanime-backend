be853bcf3aadf33000b1a807bd9194f88ab624e0
Sergio Nicolas Huaman Cheng <sergiohuamanch15@gmail.com>
2024-03-11 21:23:18 -0500

    Changes in idk

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
-const express = require("express");
-const app = express();
+import app from './app.js'
+import { sequelize } from './database/db.js'
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+async function main() {
+    try {
+        await sequelize.sync({force:false})
+        app.listen(4000);
+        console.log('Server RUN!');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
-
-// import app from './app.js'
-// import { sequelize } from './database/db.js'
-
-// async function main() {
-//     try {
-//         await sequelize.sync({force:false})
-//         app.listen(4000);
-//         console.log('Server RUN!');
-//     } catch (error) {
-//         console.error('Unable to connect to the database:', error);
-//     }
-// }
-
-// main();
+main();
