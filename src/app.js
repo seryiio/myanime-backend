3236c83c09913be069590a41f23acec4157e91ff
Sergio Nicolas Huaman Cheng <sergiohuamanch15@gmail.com>
2024-03-20 16:45:12 -0500

    CHANGE APP.JS

--- src/app.js
+++ src/app.js
@@ -13,8 +13,6 @@ import rolesRoute from './routes/roles.routes.js'
 import commentsRoute from './routes/comments.routes.js'
 import myListRoute from './routes/mylist.routes.js'
 import volumeRoute from './routes/volumes.routes.js'
-import dotenv from 'dotenv';
-dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
